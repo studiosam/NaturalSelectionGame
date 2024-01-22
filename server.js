4b77cbb0c03641f290004d0ef5baecc9d92e873c
chrisdogtn <49221396+chrisdogtn@users.noreply.github.com>
2024-01-21 20:36:19 -0500

    Adding Websocket Connections

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+var app = express();
+var server = require("http").Server(app);
+require("dotenv").config();
+
+global.io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+// Websocket Server Connection Handlers //
+io.on("connection", async (socket) => {
+  const sockets = await io.fetchSockets();
+  console.log(
+    `User Connected! ID : ${socket.id} | Total Connected Users ${sockets.length}`
+  );
+  // Handle Client Messages //
+  socket.on("logToServer", (msg) => {
+    switch (msg) {
+      case "Test":
+        console.log("Test Message");
+        break;
+
+      default:
+        console.log(msg);
+    }
+  });
+
+  // Receive Zylarian Data From Client To Send To Back To All //
+  socket.on("zylarianData", (data) => {
+    sendZylrianData(data);
+  });
+});
+
+io.on("disconnect", async (socket) => {
+  console.log(`User disconnected! ID : ${socket.id} `); // false
+});
+
+// Send Data to Client //
+
+async function logToClient(msg) {
+  await io.emit("log", msg);
+}
+
+async function sendZylrianData(data) {
+  await io.emit("ZylarianData", data);
+}
---
+++ socket.js
@@ -0,0 +1,30 @@
+// Connect client to Server Websocket //
+
+const socket = io("http://127.0.0.1:3000"); // Connect to the server immediately when the page loads
+
+// Log to console when connected //
+socket.on("connect", function () {
+  console.log(`Connected to the server as ID: ${socket.id}`);
+});
+
+// Receive Messages From Server //
+socket.on("log", async function (msg) {
+  switch (msg) {
+    case "Test":
+      console.log("Test Message");
+      break;
+  }
+});
+
+// Receive Zylarian Data Back From Server //
+socket.on("ZylarianData", async function (data) {
+  console.log("SUCCESS!!!!!", data.name);
+});
+// Send Messages To Server //
+async function logToServer(msg) {
+  socket.emit("logToServer", msg);
+}
+
+async function sendZylrianData(data) {
+  socket.emit("zylarianData", data);
+}
