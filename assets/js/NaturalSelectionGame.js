1d1f5eba91bc5799dccd10e442a63ccb307323d9
sam <studiosam423@gmail.com>
2024-01-28 19:30:23 -0500

    moved reference arrays to serverVariables from game file
    Co-authored-by: chrisdogtn <chrisdogtn@users.noreply.github.com>

--- assets/js/NaturalSelectionGame.js
+++ assets/js/NaturalSelectionGame.js
@@ -1,352 +1,6 @@
 // Zylarian Game Logic
 
 /*
-Initializing global variables
-*/
-
-let population = []; // Array of zylarians controlled by the player
-let INITIAL_ZYLARIAN = {}; //Object to hold the initial zylarian for the entire game
-let currentMate = {}; // Object to hold the current mating partner
-let generation = 0; // Keeping track of generations
-let month = 0;
-const ALPHA_H = 1;
-const ALPHA_W = 1;
-
-/* 
-Arrays of trait objects
-*/
-
-const zylarianSkinColors = [
-  {
-    id: "White",
-    beneficialEnvironment: ["Frostvale Glacier"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Arid Dunes Oasis"],
-    redGenotype: "rr",
-    greenGenotype: "gg",
-    blueGenotype: "bb",
-    brownGenotype: "pp",
-    selectable: false,
-  },
-  {
-    id: "Beige",
-    beneficialEnvironment: ["Arid Dunes Oasis"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Frostvale Glacier"],
-    redGenotype: "rr",
-    greenGenotype: "gg",
-    blueGenotype: "bb",
-    brownGenotype: "Pp",
-  },
-  {
-    id: "Gray",
-    beneficialEnvironment: ["Peaktop Highlands"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    redGenotype: "Rr",
-    greenGenotype: "Gg",
-    blueGenotype: "Bb",
-    brownGenotype: "pp",
-  },
-  {
-    id: "Blue",
-    beneficialEnvironment: ["Frostvale Glacier"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Peaktop Highlands"],
-    redGenotype: "rr",
-    greenGenotype: "gg",
-    blueGenotype: ["BB", "Bb"],
-    brownGenotype: "pp",
-    selectable: false,
-  },
-  {
-    id: "Green",
-    beneficialEnvironment: ["Verdant Canopy Island"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Frostvale Glacier"],
-    redGenotype: "rr",
-    greenGenotype: ["GG", "Gg"],
-    blueGenotype: "bb",
-    brownGenotype: "pp",
-    selectable: false,
-  },
-  {
-    id: "Red",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Frostvale Glacier"],
-    redGenotype: "RR",
-    greenGenotype: "gg",
-    blueGenotype: "bb",
-    brownGenotype: "pp",
-    selectable: false,
-  },
-  {
-    id: "Pink",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Frostvale Glacier"],
-    redGenotype: "Rr",
-    greenGenotype: "gg",
-    blueGenotype: "bb",
-    brownGenotype: "pp",
-    selectable: false,
-  },
-  {
-    id: "Yellow",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Peaktop Highlands", "Frostvale Glacier"],
-    redGenotype: ["RR", "Rr"],
-    greenGenotype: ["GG", "Gg"],
-    blueGenotype: "bb",
-    brownGenotype: "pp",
-  },
-  {
-    id: "Brown",
-    beneficialEnvironment: ["Peaktop Highlands", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Frostvale Glacier"],
-    redGenotype: ["RR", "Rr", "rr"],
-    greenGenotype: ["GG", "Gg", "gg"],
-    blueGenotype: ["BB", "Bb", "bb"],
-    brownGenotype: ["PP", "Pp"],
-  },
-  {
-    id: "Black",
-    beneficialEnvironment: ["Peaktop Highlands", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Savannah Plains Sanctuary"],
-    redGenotype: "RR",
-    greenGenotype: "GG",
-    blueGenotype: "BB",
-    brownGenotype: "PP",
-  },
-];
-const zylarianSkinTextures = [
-  {
-    id: "Scaly",
-    beneficialEnvironment: ["Arid Dunes Oasis", "Peaktop Highlands"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Frostvale Glacier"],
-    skinMoistureGenotype: "ww",
-    scaleGenotype: ["SS", "Ss"],
-    feathered: false,
-    furry: false,
-  },
-  {
-    id: "Smooth",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Frostvale Glacier"],
-    skinMoistureGenotype: ["WW", "Ww", "ww"],
-    scaleGenotype: "ss",
-    feathered: false,
-    furry: false,
-  },
-  {
-    id: "Furry",
-    beneficialEnvironment: ["Frostvale Glacier", "Savannah Plains Sanctuary"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Greenwood Falls Isle"],
-    skinMoistureGenotype: ["Ww", "ww"],
-    scaleGenotype: "ss",
-    feathered: false,
-    furry: true,
-  },
-  {
-    id: "Leathery",
-    beneficialEnvironment: ["Peaktop Highlands", "Arid Dunes Oasis"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    skinMoistureGenotype: "ww",
-    scaleGenotype: ["SS", "Ss", "ss"],
-    feathered: false,
-    furry: false,
-  },
-  {
-    id: "Feathered",
-    beneficialEnvironment: [
-      "Savannah Plains Sanctuary",
-      "Greenwood Falls Isle",
-    ],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Frostvale Glacier"],
-    skinMoistureGenotype: ["Ww", "ww"],
-    scaleGenotype: ["SS", "Ss"],
-    feathered: true,
-    furry: false,
-  },
-  {
-    id: "Slimy",
-    beneficialEnvironment: ["Greenwood Falls Isle", "Verdant Canopy Island"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Peaktop Highlands"],
-    skinMoistureGenotype: "WW",
-    scaleGenotype: ["ss"],
-    feathered: false,
-    furry: false,
-  },
-];
-const zylarianSkinPatterns = [
-  {
-    id: "Solid",
-    beneficialEnvironment: ["Peaktop Highlands", "Frostvale Glacier"],
-    detrimentalEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-  },
-  {
-    id: "Striped",
-    beneficialEnvironment: [
-      "Savannah Plains Sanctuary",
-      "Greenwood Falls Isle",
-    ],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Peaktop Highlands"],
-    selectable: false,
-  },
-  {
-    id: "Spotted",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Frostvale Glacier", "Arid Dunes Oasis"],
-    selectable: false,
-  },
-  {
-    id: "Iridescent",
-    beneficialEnvironment: ["Greenwood Falls Isle", "Frostvale Glacier"],
-    detrimentalEnvironment: ["Savannah Plains Sanctuary", "Peaktop Highlands"],
-    selectable: false,
-  },
-];
-const zylarianLimbTypes = [
-  {
-    id: "Quadripedal",
-    beneficialEnvironment: ["Savannah Plains Sanctuary", "Peaktop Highlands"],
-    detrimentalEnvironment: ["Greenwood Falls Isle", "Frostvale Glacier"],
-  },
-  {
-    id: "Bipedal Bimanual",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Savannah Plains Sanctuary"],
-  },
-  {
-    id: "Quadripedal Bimanual",
-    beneficialEnvironment: [
-      "Verdant Canopy Island",
-      "Greenwood Falls Isle",
-      "Savannah Plains Sanctuary",
-      "Peaktop Highlands",
-    ],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Savannah Plains Sanctuary"],
-    selectable: false,
-  },
-];
-const zylarianSpecialFeatures = [
-  {
-    id: "Night Vision",
-    beneficialEnvironment: [
-      "Peaktop Highlands",
-      "Greenwood Falls Isle",
-      "Arid Dunes Oasis",
-    ],
-    detrimentalEnvironment: [],
-  },
-  {
-    id: "Amphibious",
-    beneficialEnvironment: ["Greenwood Falls Isle", "Verdant Canopy Island"],
-    detrimentalEnvironment: ["Arid Dunes Oasis"],
-  },
-  {
-    id: "Enhanced Hearing",
-    beneficialEnvironment: ["Savannah Plains Sanctuary", "Frostvale Glacier"],
-    detrimentalEnvironment: [],
-  },
-  {
-    id: "Toxic Secretion",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis"],
-  },
-  {
-    id: "Webbed Feet",
-    beneficialEnvironment: ["Greenwood Falls Isle", "Verdant Canopy Island"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Peaktop Highlands"],
-  },
-  {
-    id: "Winged",
-    beneficialEnvironment: ["Frostvale Glacier", "Verdant Canopy Island"],
-    detrimentalEnvironment: [],
-  },
-  {
-    id: "Hooves",
-    beneficialEnvironment: ["Savannah Plains Sanctuary", "Peaktop Highlands"],
-    detrimentalEnvironment: ["Greenwood Falls Isle", "Frostvale Glacier"],
-  },
-  {
-    id: "Poisonous",
-    beneficialEnvironment: [],
-    detrimentalEnvironment: [],
-  },
-  {
-    id: "Venomous",
-    beneficialEnvironment: [],
-    detrimentalEnvironment: [],
-  },
-];
-const zylarianDietTypes = [
-  {
-    id: "Herbivore",
-    beneficialEnvironment: ["Verdant Canopy Island", "Greenwood Falls Isle"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Peaktop Highlands"],
-  },
-  {
-    id: "Carnivore",
-    beneficialEnvironment: ["Savannah Plains Sanctuary", "Peaktop Highlands"],
-    detrimentalEnvironment: ["Greenwood Falls Isle", "Frostvale Glacier"],
-  },
-  {
-    id: "Omnivore",
-    beneficialEnvironment: [
-      "Greenwood Falls Isle",
-      "Savannah Plains Sanctuary",
-    ],
-    detrimentalEnvironment: ["Frostvale Glacier", "Arid Dunes Oasis"],
-  },
-  {
-    id: "Insectivore",
-    beneficialEnvironment: ["Verdant Canopy Island", "Peaktop Highlands"],
-    detrimentalEnvironment: ["Frostvale Glacier", "Arid Dunes Oasis"],
-  },
-  {
-    id: "Piscivore",
-    beneficialEnvironment: ["Greenwood Falls Isle", "Frostvale Glacier"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Peaktop Highlands"],
-  },
-  {
-    id: "Nectarivore",
-    beneficialEnvironment: ["Greenwood Falls Isle", "Verdant Canopy Island"],
-    detrimentalEnvironment: ["Arid Dunes Oasis", "Savannah Plains Sanctuary"],
-  },
-];
-optionsMapping = {
-  // Mapping for the original Zylarian form
-  skinColor: zylarianSkinColors,
-  skinTexture: zylarianSkinTextures,
-  skinPattern: zylarianSkinPatterns,
-  limbType: zylarianLimbTypes,
-  specialFeatures: zylarianSpecialFeatures,
-  dietType: zylarianDietTypes,
-
-  // Mapping for the mate form
-  mateSkinColor: zylarianSkinColors,
-  mateSkinTexture: zylarianSkinTextures,
-  mateSkinPattern: zylarianSkinPatterns,
-  mateLimbType: zylarianLimbTypes,
-  mateSpecialFeatures: zylarianSpecialFeatures,
-  mateDietType: zylarianDietTypes,
-};
-
-/*
-Arrays of traits with multiple type options. -- Used for populating dropdown menus. Probably could be optimized
-*/
-
-const variablesWithOptions = [
-  "skinColor",
-  "skinTexture",
-  "limbType",
-  "specialFeatures",
-  "dietType",
-];
-const mateVariablesWithOptions = [
-  "mateSkinTexture",
-  "mateSkinPattern",
-  "mateLimbType",
-  "mateSpecialFeatures",
-  "mateDietType",
-];
-
-/*
 Creating new zylarians
 */
 /* 
@@ -587,6 +241,45 @@ Utility Functions
 function getZylarianByName(name) {
   console.log("Getting zylarian by name");
   return population.find((zylarian) => zylarian.name === name);
+}
+
+// Returns a string of all color genotypes
+function joinColorAlleles(colorGenotypes) {
+  return (
+    colorGenotypes.redGenotype +
+    colorGenotypes.greenGenotype +
+    colorGenotypes.blueGenotype +
+    colorGenotypes.brownGenotype
+  );
+}
+
+// Returns a string of unique capital letters from a string
+function findUniqueCapitalLetters(str) {
+  const uniqueCapitals = new Set();
+
+  for (const char of str) {
+    if (char >= "A" && char <= "Z") {
+      uniqueCapitals.add(char);
+    }
+  }
+  return Array.from(uniqueCapitals).join("");
+}
+
+// Returns a skin pattern based on a string of unique capital letters
+function skinPatternDetermination(dominantAlleles) {
+  if (dominantAlleles.length > 1) {
+    if (dominantAlleles == "GB" || dominantAlleles == "BG") {
+      return zylarianSkinPatterns[3].id;
+    }
+    let randomNum = Math.random();
+    if (randomNum > 0.7) {
+      return zylarianSkinPatterns[1].id;
+    } else if (randomNum > 0.3) {
+      return zylarianSkinPatterns[2].id;
+    } else {
+      return zylarianSkinPatterns[0].id;
+    }
+  }
 }
 
 // Returns a random name
--- assets/js/serverVariables.js
+++ assets/js/serverVariables.js
@@ -1,2 +1,254 @@
 const serverAddress = "http://127.0.0.1:3000/";
 // const serverAddress = "https://nsgserver.gbs423.com/";
+
+/*
+Initializing global variables
+*/
+
+let population = []; // Array of zylarians controlled by the player
+let INITIAL_ZYLARIAN = {}; //Object to hold the initial zylarian for the entire game
+let currentMate = {}; // Object to hold the current mating partner
+let generation = 0; // Keeping track of generations
+let month = 0;
+const ALPHA_H = 1;
+const ALPHA_W = 1;
+
+/* 
+Arrays of trait objects
+*/
+
+const zylarianSkinColors = [
+  {
+    id: "White",
+    redGenotype: "rr",
+    greenGenotype: "gg",
+    blueGenotype: "bb",
+    brownGenotype: "pp",
+    selectable: false,
+  },
+  {
+    id: "Beige",
+    redGenotype: "rr",
+    greenGenotype: "gg",
+    blueGenotype: "bb",
+    brownGenotype: "Pp",
+  },
+  {
+    id: "Gray",
+    redGenotype: "Rr",
+    greenGenotype: "Gg",
+    blueGenotype: "Bb",
+    brownGenotype: "pp",
+  },
+  {
+    id: "Blue",
+    redGenotype: "rr",
+    greenGenotype: "gg",
+    blueGenotype: ["BB", "Bb"],
+    brownGenotype: "pp",
+    selectable: false,
+  },
+  {
+    id: "Green",
+    redGenotype: "rr",
+    greenGenotype: ["GG", "Gg"],
+    blueGenotype: "bb",
+    brownGenotype: "pp",
+    selectable: false,
+  },
+  {
+    id: "Red",
+    redGenotype: "RR",
+    greenGenotype: "gg",
+    blueGenotype: "bb",
+    brownGenotype: "pp",
+    selectable: false,
+  },
+  {
+    id: "Pink",
+    redGenotype: "Rr",
+    greenGenotype: "gg",
+    blueGenotype: "bb",
+    brownGenotype: "pp",
+    selectable: false,
+  },
+  {
+    id: "Yellow",
+    redGenotype: ["RR", "Rr"],
+    greenGenotype: ["GG", "Gg"],
+    blueGenotype: "bb",
+    brownGenotype: "pp",
+  },
+  {
+    id: "Brown",
+    redGenotype: ["RR", "Rr", "rr"],
+    greenGenotype: ["GG", "Gg", "gg"],
+    blueGenotype: ["BB", "Bb", "bb"],
+    brownGenotype: ["PP", "Pp"],
+  },
+  {
+    id: "Black",
+    redGenotype: "RR",
+    greenGenotype: "GG",
+    blueGenotype: "BB",
+    brownGenotype: "PP",
+  },
+];
+const zylarianSkinTextures = [
+  {
+    id: "Scaly",
+    skinMoistureGenotype: "ww",
+    scaleGenotype: ["SS", "Ss"],
+    feathered: false,
+    furry: false,
+  },
+  {
+    id: "Smooth",
+    skinMoistureGenotype: ["WW", "Ww", "ww"],
+    scaleGenotype: "ss",
+    feathered: false,
+    furry: false,
+  },
+  {
+    id: "Furry",
+    skinMoistureGenotype: ["Ww", "ww"],
+    scaleGenotype: "ss",
+    feathered: false,
+    furry: true,
+  },
+  {
+    id: "Leathery",
+    skinMoistureGenotype: "ww",
+    scaleGenotype: ["SS", "Ss", "ss"],
+    feathered: false,
+    furry: false,
+  },
+  {
+    id: "Feathered",
+    skinMoistureGenotype: ["Ww", "ww"],
+    scaleGenotype: ["SS", "Ss"],
+    feathered: true,
+    furry: false,
+  },
+  {
+    id: "Slimy",
+    skinMoistureGenotype: "WW",
+    scaleGenotype: ["ss"],
+    feathered: false,
+    furry: false,
+  },
+];
+const zylarianSkinPatterns = [
+  {
+    id: "Solid",
+  },
+  {
+    id: "Striped",
+    selectable: false,
+  },
+  {
+    id: "Spotted",
+    selectable: false,
+  },
+  {
+    id: "Iridescent",
+    selectable: false,
+  },
+];
+const zylarianLimbTypes = [
+  {
+    id: "Quadripedal",
+  },
+  {
+    id: "Bipedal Bimanual",
+  },
+  {
+    id: "Quadripedal Bimanual",
+    selectable: false,
+  },
+];
+const zylarianSpecialFeatures = [
+  {
+    id: "Night Vision",
+  },
+  {
+    id: "Amphibious",
+  },
+  {
+    id: "Enhanced Hearing",
+  },
+  {
+    id: "Toxic Secretion",
+  },
+  {
+    id: "Webbed Feet",
+  },
+  {
+    id: "Winged",
+  },
+  {
+    id: "Hooves",
+  },
+  {
+    id: "Poisonous",
+  },
+  {
+    id: "Venomous",
+  },
+];
+const zylarianDietTypes = [
+  {
+    id: "Herbivore",
+  },
+  {
+    id: "Carnivore",
+  },
+  {
+    id: "Omnivore",
+  },
+  {
+    id: "Insectivore",
+  },
+  {
+    id: "Piscivore",
+  },
+  {
+    id: "Nectarivore",
+  },
+];
+optionsMapping = {
+  // Mapping for the original Zylarian form
+  skinColor: zylarianSkinColors,
+  skinTexture: zylarianSkinTextures,
+  skinPattern: zylarianSkinPatterns,
+  limbType: zylarianLimbTypes,
+  specialFeatures: zylarianSpecialFeatures,
+  dietType: zylarianDietTypes,
+
+  // Mapping for the mate form
+  mateSkinColor: zylarianSkinColors,
+  mateSkinTexture: zylarianSkinTextures,
+  mateSkinPattern: zylarianSkinPatterns,
+  mateLimbType: zylarianLimbTypes,
+  mateSpecialFeatures: zylarianSpecialFeatures,
+  mateDietType: zylarianDietTypes,
+};
+
+/*
+Arrays of traits with multiple type options. -- Used for populating dropdown menus. Probably could be optimized
+*/
+
+const variablesWithOptions = [
+  "skinColor",
+  "skinTexture",
+  "limbType",
+  "specialFeatures",
+  "dietType",
+];
+const mateVariablesWithOptions = [
+  "mateSkinTexture",
+  "mateSkinPattern",
+  "mateLimbType",
+  "mateSpecialFeatures",
+  "mateDietType",
+];
